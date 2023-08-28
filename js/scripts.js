83bc6cf2d32c563726909b2d02d39d3e46088a0e
Sara Zacarias Cox <94139313+sarazcox@users.noreply.github.com>
2023-08-27 23:21:58 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,71 @@
-console.log('javascript connected!');
-    
-const carousel = new bootstrap.Carousel('#homeCarousel', {
-    interval: 2000,
-    pause: false
+console.log("javascript connected!");
+
+const carousel = new bootstrap.Carousel("#homeCarousel", {
+  interval: 2000,
+  pause: false,
 });
 
-// when the pause button is clicked, pause the carousel
-const carouselPause = document.getElementById('carouselPause');
-carouselPause.addEventListener('click', function() {
-    console.log('pausing the carousel');
-    carousel.pause();
-})
+const carouselButton = document.getElementById("carouselButton");
+const faIcon = document.getElementById("faButton");
 
-// when the play button is clicked, begin cycling through the images
-const carouselPlay = document.getElementById('carouselPlay');
-carouselPlay.addEventListener('click', function() {
-    console.log('cycle the carousel');
+carouselButton.addEventListener("click", function () {
+  if (faIcon.classList.contains("fa-pause")) {
+    faIcon.classList.remove("fa-pause");
+    faIcon.classList.add("fa-play");
+    carousel.pause();
+  } else {
+    faIcon.classList.remove("fa-play");
+    faIcon.classList.add("fa-pause");
     carousel.cycle();
-})
+  }
+});
+
+async function fetchWeather() {
+  try {
+    const apiKey = process.env.OPEN_WEATHER_API_KEY;
+    const city = "New York";
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+    displayWeather(data);
+  } catch (error) {
+    console.error(`There was an error!`, error);
+  } finally {
+    console.log("Fetched response");
+  }
+}
+
+fetchWeather();
+
+function displayWeather(weatherData) {
+  const weatherIcon = document.querySelector("#weather-icon");
+  const weatherTemp = document.querySelector("#weather-temp");
+  const weatherDescription = document.querySelector("#weather-description");
+  const cityWeather = document.querySelector("#city-weather");
+
+  const icon = document.createElement("img");
+  icon.src = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+  weatherIcon.appendChild(icon);
+
+//    temp in fahrenheit
+  // const temp = document.createElement("h6");
+  // temp.textContent = `${weatherData.main.temp.toFixed(1)}\u00B0F`;
+  // weatherTemp.appendChild(temp);
+
+//   temp in celsius 
+  const tempFahrenheit = weatherData.main.temp;
+  const tempCelsius = (((tempFahrenheit - 32) * 5) / 9).toFixed(1);
+  const temp = document.createElement("h6");
+  temp.textContent = `${tempCelsius}\u00B0C`;
+  weatherTemp.appendChild(temp);
+
+  const description = document.createElement("h6");
+  description.textContent = `${weatherData.weather[0].description}`;
+  weatherDescription.appendChild(description);
+
+  const cityName = document.createElement("h6")
+  cityName.textContent = `-- ${weatherData.name}`;
+  cityWeather.appendChild(cityName);
+
+}
